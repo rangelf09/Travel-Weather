6aba754066e654fa046e72d5bc0b358ffcf0e355
Cindy Paredes <cindyrangel@Cindys-iMac.local>
2022-12-03 20:15:25 -0600

    added some changes was not working now the dashboard works but cannot get date on there

--- assets/index.js
+++ assets/index.js
@@ -1,20 +1,20 @@
 
 // button to search
 function searchWeather () {
-    var apiKey = "c95e9c9171f928b48494c0f3b0126e89";
+    var apiKey = "f9e3ef72f0eaeee2c3f8a3b8951dd135";
 
    let dataCity = document.querySelector("#search input").value;
 
    let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + dataCity + "&appid=" + apiKey;
-   fetch(weather)
+   fetch(apiUrl)
    .then(function(response){
     if(response.ok) {
         response.json()
         .then(function(data) {
             let lat = (data.coord.lat);
-            let lon = (data.coord.long);
+            let lon = (data.coord.lon);
             let dt = (data.dt)
-            weather(dataCity, dt, lat, long, apiKey);
+            weather(dataCity, dt, lat, lon, apiKey);
         });
     } else {
         alert("error");
@@ -22,13 +22,13 @@ function searchWeather () {
    });
 };
 
-function weather(dataCity, dt, lat, long, apiKey) {
+function weather(dataCity, dt, lat, lon, apiKey) {
     let city = {
         today: {},
         forecast: {}
-    }
+    };
+    let oneCallApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&dt=" + dt + "&units=imperial&appid=" + apiKey;
 
-    let oneCallApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&dt=" + dt + "&units=imperial&appid=" + yekipa;
 // fetch weather specs
     fetch(oneCallApi).then(function(response) {
         if (response.ok) {
@@ -36,7 +36,7 @@ function weather(dataCity, dt, lat, long, apiKey) {
                 console.log(data)
                 city.today = {
                     data: String(new Date(dt * 1000)),
-                    icon: dataCity.current.weather[0].icon,
+                    // icon: dataCity.current.weather[0].icon,
                     temp: data.current.temp,
                     wind: data.current.wind_speed,
                     humidity: data.current.humidity
@@ -48,13 +48,12 @@ function weather(dataCity, dt, lat, long, apiKey) {
                         icon: data.daily[i].weather[0].icon,
                         temp: data.daily[i].temp.day,
                         wind: data.daily[i].wind_speed,
-                        humidity: data.daily[i.humidity]
+                        humidity: data.daily[i].humidity
                     }
                 }
-                namedCities = city
-                localStorage.setItems('namedCities', json.stringify(namedCities));
-
-                displayWeather(namedCities)
+                
+                
+                displayWeather(city)
             })
         }
     });
@@ -62,7 +61,7 @@ function weather(dataCity, dt, lat, long, apiKey) {
 
 function displayWeather (city) {
     let dateEl = document.createElement('h2')
-    dateEl.innerText =(namedCities[city].today.date).substr(0, 10)
+    dateEl.innerText =(city.today)
     document.getElementById("today").appendChild(dateEl)
 
     // let imageEl = document.createElement('img')
@@ -70,15 +69,19 @@ function displayWeather (city) {
     // dateEl.append(imageEl);
 
     let tempEl = document.createElement('h3')
-    tempEl.innerText ="Temp: " + namedCities[city].today.temp + " Fahrenheit"
+    tempEl.innerText ="Temp: " + city.today.temp + " Fahrenheit"
     document.getElementById("today").appendChild(tempEl)
 
     let windEl = document.createElement('h3')
-    windEl.innerText ="Wind: " + namedCities[city].today.temp + " MPH"
+    windEl.innerText ="Wind: " + city.today.temp + " MPH"
     document.getElementById("today").appendChild(windEl)
 
     let humidityEl = document.createElement('h3')
-    humidityEl.innerText ="Humidity: " + namedCities[city].today.temp + " %"
+    humidityEl.innerText ="Humidity: " + city.today.temp + " %"
     document.getElementById("today").appendChild(humidityEl)
+
+
+
+    // forecast
  
 }
