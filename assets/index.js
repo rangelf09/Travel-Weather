a87f35a52b8b0084555631871a9d2aa724c9b9bf
Cindy Paredes <cindyrangel@Cindys-iMac.local>
2022-12-03 18:41:01 -0600

    added specs to fetch data from api

--- assets/index.js
+++ assets/index.js
@@ -14,9 +14,48 @@ function searchWeather () {
             let lat = (data.coord.lat);
             let lon = (data.coord.long);
             let dt = (data.dt)
+            weather(dataCity, dt, lat, long, apiKey);
         });
     } else {
         alert("error");
     }
    });
 };
+
+function weather(dataCity, dt, lat, long, apiKey) {
+    let city = {
+        today: {},
+        forecast: {}
+    }
+
+    let oneCallApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&dt=" + dt + "&units=imperial&appid=" + yekipa;
+// fetch weather specs
+    fetch(oneCallApi).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data)
+                city.today = {
+                    data: String(new Date(dt * 1000)),
+                    icon: dataCity.current.weather[0].icon,
+                    temp: data.current.temp,
+                    wind: data.current.wind_speed,
+                    humidity: data.current.humidity
+                };
+                // for the week forecast
+                for (i = 1; i < 6; i++) {
+                    city.forecast[i] = {
+                        dat: String(new Date (data.daily[i].dt * 1000)),
+                        icon: data.daily[i].weather[0].icon,
+                        temp: data.daily[i].temp.day,
+                        wind: data.daily[i].wind_speed,
+                        humidity: data.daily[i.humidity]
+                    }
+                }
+                namedCities = city
+                localStorage.setItems('namedCities', json.stringify(namedCities));
+
+                displayWeather(namedCities)
+            })
+        }
+    });
+}
